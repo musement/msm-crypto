7f73b7f1f3869f211d11a83e74ba08b0c864597d
Michele Riva <master@micheleriva.it>
2019-03-04 16:11:57 +0100

    improved code formatting

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,12 +1,12 @@
 const path = require("path");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
   output: {
-    filename: 'msm-crypto.bundle.js',
-    path: path.resolve(__dirname, '../dist'),
-    library: 'msmCrypto'
+    filename: "msm-crypto.bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    library: "msmCrypto"
   },
   module: {
     rules: [
@@ -20,6 +20,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(["dist"])
   ]
 };
--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,8 +1,8 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
   watch: true,
-  devtool: 'inline-source-map'
+  devtool: "inline-source-map"
 });
